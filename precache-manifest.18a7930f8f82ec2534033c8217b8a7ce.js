74d2c7ecb916119ea873ebf33a56a5b003f2759c
Daniel TORSCHO <daniel.torscho@numberly.com>
2018-09-20 14:26:47 +0200

    build folder to docs folder

---
+++ precache-manifest.18a7930f8f82ec2534033c8217b8a7ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7a85659624eed566a6cd",
+    "url": "/js/chunk-vendors.a48b8ef9.js"
+  },
+  {
+    "revision": "83bb242eefc690aee505",
+    "url": "/js/app.976042a2.js"
+  },
+  {
+    "revision": "dd00af6872e721d2ee68ac0298198bf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "83bb242eefc690aee505",
+    "url": "/css/app.97e1b30b.css"
+  }
+];
